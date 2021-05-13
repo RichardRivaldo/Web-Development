646d5b71be9fd09174af78f43551bf6e26b8d4bb
RichardRivaldo <richardrivaldo84@gmail.com>
2021-05-14 00:01:58 +0700

    Added notes on JS functions

---
+++ Notes/functions.js
@@ -0,0 +1,24 @@
+/* Callbacks and Higher Order Function */
+
+// Example -> function in the forEach or addEventListeners -> Callbacks
+// Functions that executions are not immediately, but after a certain events occur
+
+// Higher Order Function
+// In short, a function that calls and use another function
+// function thisFunctionCalls(AnotherFunction)
+
+//----------------------------------------------------------------------------------------------------------------//
+
+/* Anonymous Function */
+
+// Function declared without any name for it
+// Usually a one-time used function
+
+//----------------------------------------------------------------------------------------------------------------//
+
+/* Arrow Function */
+
+// Because there are many callbacks function, arrow functions are made for simpler representation
+// No need to use the keyword function, just need to specify the parameters used in the function
+// For simple functions, we can also try not to use {}. Everything after the => will be returned
+// No Parameters -> just use ()
