17f0762f2d6e10f0c5b503a096f6b8ddecbc2264
RichardRivaldo <richardrivaldo84@gmail.com>
2021-05-20 21:53:13 +0700

    JS IIFE

--- Notes/misc.js
+++ Notes/misc.js
@@ -17,3 +17,9 @@
 // };
 // const {attr3} = obj, attr3 = val3
 // const {attr5, attr6} = obj.attr4
+
+/* IIFE (Immediately Invoked Function Expression) */
+
+// A function that immediately gets invoked upon declaration
+// Mostly found on libraries to prevent the function from making global variables
+// Syntax -> (function(){})();
