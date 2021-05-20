6ae25f37dc5990bd30f57bc1c7ae2764369d1b21
RichardRivaldo <richardrivaldo84@gmail.com>
2021-05-20 23:15:31 +0700

    JS Async-Await

---
+++ Notes/async.js
@@ -0,0 +1,13 @@
+/* Async and Await */
+
+// Async and Await are syntactic sugar of promises
+// Because of these two functionalities, the code will be a lot cleaner and looks totally like sync codes
+// The background process is the same as promises
+
+// To declare an async function, use `async function functionName()`
+// To call async functions, use `await functions()`
+
+/* Error Handling */
+
+// Wrap the async callers in a try-catch block
+// The catch will catch the errors thrown by the function
