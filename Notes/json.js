4b25981a913ded76dd37d5730a34ae3e8c4fadc0
RichardRivaldo <richardrivaldo84@gmail.com>
2021-05-20 23:26:18 +0700

    JS JSON and Fetch API

---
+++ Notes/json.js
@@ -0,0 +1,13 @@
+/* JSON */
+
+// A JSON is basically JS objects, can also be array of objects
+// The JS then can communicate with other languages by trading objects
+
+/* Fetch */
+
+// Fetch can be used to get data from APIs and mostly it returns us a JSON object
+// fetch(fetchedAPI)
+// Fetch is a promise, and we can use .then() to use callbacks after the fetching is done
+
+// We can convert the JSON object to JS Object with .json()
+// The method is also a promise
