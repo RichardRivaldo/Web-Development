4bbb2833186a9dc8d46deb17f99d50419288bc1b
RichardRivaldo <richardrivaldo84@gmail.com>
2021-05-20 22:22:48 +0700

    JS Closures and Modules

--- Notes/misc.js
+++ Notes/misc.js
@@ -23,3 +23,30 @@
 // A function that immediately gets invoked upon declaration
 // Mostly found on libraries to prevent the function from making global variables
 // Syntax -> (function(){})();
+
+/* Closures */
+
+// function outer(outerVar) {
+//     return function inner(innerVar) {
+//         console.log(outerVar);
+//         console.log(innerVar);
+//     };
+// }
+// const newFunc = outer("Outer");
+// newFunc("Inner");
+
+// The inner function always has access to variables and scopes of its outer function
+// Even after the variable defined for the outer function is no longer accessible
+
+/* Modules */
+
+// There are 2 methods of import-export in JS
+
+// Default exports -> export only one values or functions from one module with arbitrary names
+// export default values
+// import values
+
+// Named exports -> export several values or functions from one module with fixed names
+// export const values or export {value1, value2}
+// import {values as alias}
+// Can use alias as to change the values' names
